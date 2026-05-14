24324b7130d2b3b7adc59128503ad983366e1412
pirgus <jaquecavaller@gmail.com>
2026-05-14 14:10:30 -0300

    addition of different versions of the arm dh params

--- inmoov_arm_test.py
+++ inmoov_arm_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import roboticstoolbox as rtb
 import numpy as np
 import spatialmath as sp
-import random
+# import random
 
 # creation of the left arm based on inmoov's DH parameters
 # left_arm = rtb.DHRobot(
@@ -18,7 +18,7 @@ import random
 #     name="LeftArm"
 # )
 
-# the version below is the one contained in Kenshimov's article
+# the version below is the one presented on Kenshimov's article
 left_arm = rtb.DHRobot(
     [
         rtb.RevoluteDH(alpha=(np.pi)/2,  a=0.2,    d=0,     offset=0),
@@ -31,24 +31,39 @@ left_arm = rtb.DHRobot(
     name="LeftArm"
 )
 
+# 4dof version of the inmoov arm by Abdelaziz
+# (doesn't include the wrist)
+arm = rtb.DHRobot(
+    [
+        rtb.RevoluteDH(alpha=np.pi/2, a=0.05298, d=0, offset=-(np.pi)/2),
+        rtb.RevoluteDH(alpha=np.pi/2, a=0, d=0.13219, offset=np.pi/2),
+        rtb.RevoluteDH(alpha=-(np.pi)/2, a=0, d=0.27585, offset=0),
+        rtb.RevoluteDH(alpha=0, a=np.sqrt(0.350**2 + 0.05**2), d=0, offset=1.4289)
+    ]
+)
+
+angles_alt = [0, 0, 0, 0]
+arm.base = sp.SE3(0.2, 0, 0) * sp.SE3.Rx(np.pi/2)
+angles = np.array([[0, 0, 0, 0, 0, 0], [0, 0, np.pi/4, 0, 0, 0], [0, 0, np.pi/2, 0, 0, 0]])
 # (fixed) base's position adjustment (fixed)
 left_arm.base = sp.SE3(0.2, 0, 0) * sp.SE3.Rx(np.pi/2)
 
 b = left_arm.base.t
 
 # generating a random angles vector between -90º and 90º for the five joints
-angles = [random.uniform(-np.pi/2, np.pi/2) for _ in range(6)]
+# angles = [random.uniform(-np.pi/2, np.pi/2) for _ in range(6)]
 # angles = [0, np.pi/2, np.pi/4, 0, 0]
 print(angles)
 forward = left_arm.fkine(angles)
-print("forwardk = \n", forward)
+# print("forwardk = \n", forward)
 Y = sp.SE3(0.3, 0.2, -0.1)
-T = left_arm.ikine_LM(forward)
-print(T)
-left_arm.plot(T.q, block=False)
+# T = left_arm.ikine_LM(forward)
+# print(T)
+left_arm.plot(angles, block=False, dt=1)
+# arm.plot(angles_alt, block=False)
 ax = plt.gca()
 ax.scatter(b[0], b[1], b[2], s=100, c='red', label='base')
-ax.scatter(forward.t[0], forward.t[1], forward.t[2], s=80, c='blue', label='goal')
+# ax.scatter(forward.t[0], forward.t[1], forward.t[2], s=80, c='blue', label='goal')
 ax.legend()
 # plt.scatter(b[0], b[1], b[2], s=100, c='red', label='Base')
 # plt.legend()
